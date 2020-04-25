445c47519ee6dae46eae4205afb5c7c3f9de3254
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-04-25 12:38:45 +0200

    🚚 Rename files

--- litex/litex.py
+++ litex/litex.py
@@ -2,7 +2,7 @@ from os import path
 from lark import Lark, Tree
 
 def parse(contents) -> Tree:
-  grammar: str = open(path.join(path.dirname(__file__), 'grammar.lark'))
+  grammar: str = open(path.join(path.dirname(__file__), 'grammar.lark')).read()
   parser: Lark = Lark(grammar, start="start")
   return parser.parse(contents)
 
