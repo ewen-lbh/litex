79839425b271542983372037249c0c273085362a
Ewen Le Bihan <hey@ewen.works>
2022-08-10 23:49:14 +0200

    🚧 Improve output

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,13 @@ use std::io::{self, BufRead};
 fn main() {
     for line in io::stdin().lock().lines() {
         let l = line.unwrap();
+        println!("{}", &l);
         if let Ok((_, expr)) = expression(InputState::new(), &l) {
-            println!("{}\t\t\t~~> {}", &l, expr.emit_keep_parens());
+            println!("{:#?}", expr);
+            println!("{}", expr.emit_keep_parens());
         } else {
             println!("{}\t\t\t~~> an error occured for this line", &l);
         }
+        println!("");
     }
 }
